6e40bcf2aad8d8b2c3c98a33c24bb1b14ff8b8ef
Khloe Hou <khou@e1z3r2p3.42.us.org>
2018-11-03 16:46:34 -0700

    11/03/2018

--- srcs/plong.c
+++ srcs/plong.c
@@ -6,7 +6,7 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 13:45:49 by khou              #+#    #+#             */
-/*   Updated: 2018/11/02 16:37:16 by khou             ###   ########.fr       */
+/*   Updated: 2018/11/03 16:44:40 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,47 @@ char	fType(int n)
 	return (c);
 }
 
+char	*fPermission(int n)
+{
+	char	*perm = ft_strnew(11);
+
+	perm[0] =  fType(n & S_IFMT);
+	perm[1] = (n & S_IRUSR) ? 'r' : '-';
+	perm[2] = (n & S_IWUSR) ? 'w' : '-';
+	perm[3] = (n & S_IXUSR) ?
+                  ((n & S_ISUID) ? 's' : 'x') :
+                  ((n & S_ISUID) ? 'S' : '-');
+	perm[4] = (n & S_IRGRP) ? 'r' : '-';
+	perm[5] = (n & S_IWGRP) ? 'w' : '-';
+	perm[6] = (n & S_IXGRP) ?
+                  ((n & S_ISGID) ? 's' : 'x') :
+                  ((n & S_ISGID) ? 'S' : '-');
+	perm[7] = (n & S_IROTH) ? 'r' : '-';
+	perm[8] = (n & S_IWOTH) ? 'w' : '-';
+	perm[9] = (n & S_IXOTH) ? 'x' : '-';
+	perm[10] = '@';
+	perm[11] = '\0';
+	if (perm[0] == '-' && perm[3] == perm[6] &&
+		perm[6] == perm[9] && perm[9] == 'x')
+		perm[0] = 's';
+	ft_printf("%s ", perm);
+	return (perm);
+}
+
 void	ls_fmt( t_lsflags *store, t_node *tree)
 {
 	struct stat		sb;
 	struct passwd	*p;
 	struct group	*grp;
 //	char			*fname;
+	char	*permission = NULL;
 
 //	ft_printf("                     WROKING on: %s\n", tree->fullpath);
 	lstat(tree->fullpath, &sb);
 	if (store->l)
 	{
 /*--permission--*/
-		ft_printf("%c%c%c%c%c%c%c%c%c%c%c ",
+/*		ft_printf("%c%c%c%c%c%c%c%c%c%c%c ",
 				  fType(sb.st_mode & S_IFMT),//how to mask
 				  (sb.st_mode & S_IRUSR) ? 'r' : '-',
 				  (sb.st_mode & S_IWUSR) ? 'w' : '-',
@@ -63,6 +91,9 @@ void	ls_fmt( t_lsflags *store, t_node *tree)
 				  (sb.st_mode & S_IXOTH) ? 'x' : '-',
 				  '@'
 			);
+*/
+		permission = fPermission(sb.st_mode);
+		
 /*--Link count--*/
 		ft_printf("%ld", (long) sb.st_nlink);
 		ft_printf(" ");
@@ -85,13 +116,15 @@ void	ls_fmt( t_lsflags *store, t_node *tree)
 		ft_printf("%.5s ", ctime(&sb.st_mtime)+11);
 	}
 /*--File Name--*/
-	pFname(fType(sb.st_mode & S_IFMT), tree->fullpath);//how to mask
+	pFname(permission[0], tree->fullpath);//how to mask
+	free (permission);
 }
 
 void	pFname(char c, char *path)
 {
 	char	*fname;
 	char	*color;
+	ssize_t	ret;
 	char	*bypath = NULL;
 //	struct stat	sb;
 	
@@ -100,9 +133,14 @@ void	pFname(char c, char *path)
 	else if (c == 'l')
 	{
 		color = KMAG;
-		readlink(path, bypath, 5000);
+		ret = readlink(path, bypath, 5000);
+		ft_printf("readlink buff: %ld\n", ret);
 		ft_printf("bypath: %s\n", bypath);
 	}
+	else if (c == 's')
+		color = KGRN;
+	else if (c == 'p')
+		color = KYEL;
 	else
 		color = KNRM;
 
